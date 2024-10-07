d59a25dcec76c8d3cf31272ea5d7a8f1cee7c3c6
arsahak <arsahak99@gmail.com>
2024-10-07 12:51:58 +0600

    update data

--- .next/static/webpack/webpack.0f8b45e58f6c2c5a.hot-update.js
+++ .next/static/webpack/webpack.0f8b45e58f6c2c5a.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "63f42604efd16b7d"; }
+/******/ 	__webpack_require__.h = function() { return "b47c8ce47c0836b7"; }
 /******/ }();
 /******/ 
 /******/ }
