7080ea3fe74fad70349d68c7548fd4eb55a4ea45
arsahak <arsahak99@gmail.com>
2024-10-07 12:15:20 +0600

    update public url api

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -166,7 +166,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6ca6521b06be5473"; }
+/******/ 		__webpack_require__.h = function() { return "63b33da9658cbd62"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
